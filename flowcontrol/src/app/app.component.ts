00ad49f73ce93a67563fd7a0d71259676944305e
nbnchy <nbnchy049@gmail.com>
2025-06-20 17:16:39 +0545

    flow control

---
+++ flowcontrol/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']  // ✅ Fixed: use 'styleUrls'
+})
+export class AppComponent {
+  display = true;
+  toggleDiv=true;
+
+  hide() {
+    this.display = false;
+  }
+
+  show() {
+    this.display = true;
+  }
+
+  toggle() {
+    this.display = !this.display;
+  }
+
+  toggleTwo(){
+
+    this.toggleDiv=!this.toggleDiv
+  }
+}
